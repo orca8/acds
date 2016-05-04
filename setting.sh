65f1a0bd103d9c31e5a4d962955b144d8eb9806c
orca8 <orca8@users.noreply.github.com>
2016-05-04 23:27:41 +0900

    (refs #6)Vagrantのsynced_folderの影響を回避するためmktempを利用

--- setting.sh
+++ setting.sh
@@ -5,5 +5,8 @@ set -euv
 docker cp deploy:/root/.ssh/id_rsa.pub ~
 chmod 0600 ~/id_rsa.pub
 docker cp ~/id_rsa.pub setup:/root/.ssh/authorized_keys
-chmod 0600 ssh-config
-docker cp ssh-config deploy:/root/.ssh/config
+
+temp=`mktemp`
+cp ssh-config ${temp}
+chmod 0600 ${temp}
+docker cp ${temp} deploy:/root/.ssh/config
