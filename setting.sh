3e2cfa16ffc44e634a2548f9518f59927e2bd03b
orca8 <orca8@users.noreply.github.com>
2016-05-04 22:46:58 +0900

    (refs #6).ssh/configはコンテキストの範囲を考慮して`docker cp`で行う

--- setting.sh
+++ setting.sh
@@ -5,3 +5,5 @@ set -euv
 docker cp deploy:/root/.ssh/id_rsa.pub ~
 chmod 0600 ~/id_rsa.pub
 docker cp ~/id_rsa.pub setup:/root/.ssh/authorized_keys
+chmod 0600 ssh-config
+docker cp ssh-config deploy:/root/.ssh/config
